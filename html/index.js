578abadd937a2534b9fe479ec4b491f6d442f1a7
01-AlstonAlvares <112925234+01-AlstonAlvares@users.noreply.github.com>
2022-12-10 19:13:08 +0530

    Update index.js

--- html/index.js
+++ html/index.js
@@ -1,3 +1,4 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 
 console.log(countEl)
@@ -7,4 +8,11 @@ function increment() {
    count = count + 1
    countEl.innerText = count
    console.log(count)
+}
+function save()
+{
+   let countStr = count + " - "
+   saveEl.innerText += countStr
+   countEl.textContent = 0
+   count = 0
 }
